0dfac688004ae6e923c18de881a0e538b4103933
Ramilia3110 <37445741+Ramilia3110@users.noreply.github.com>
2023-03-25 21:03:54 +0300

    Add files via upload
    animated nabber

--- aTech/js/script.js
+++ aTech/js/script.js
@@ -1,16 +1,19 @@
+let menu = document.querySelector("#menu-bar");
+let navbar = document.querySelector(".link");
+
+navbar.onclick = () => {
+  var logo = this.document.querySelector(".logo");
+  logo.classList.toggle("moveRight");
+  var link = this.document.querySelector(".link");
+  link.classList.toggle("moveLeft");
+};
+
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
+
 window.addEventListener("scroll", function () {
   var header = this.document.querySelector("header");
   header.classList.toggle("sticky", this.window.scrollY > 0);
-});
-
-let links = document.querySelector(".link");
-
-window.addEventListener("click", function () {
-  var logo = this.document.querySelector(".logo");
-  logo.classList.toggle("moveRight");
-});
-
-window.addEventListener("click", function () {
-  var link = this.document.querySelector(".link");
-  link.classList.toggle("moveLeft");
 });
